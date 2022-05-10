a9b0ab0bedcaeb08d665a46c28b23a994d899a68
dev-ahmedali <ahmedali69325@gmail.com>
2022-05-11 02:08:50 +0600

    added navbar

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,16 @@
 import Head from 'next/head'
-import Image from 'next/image'
 import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div className={styles.container}>
+      
       <Head>
         <title>Next Restaurant</title>
         <meta name="description" content="Best Pizza shop in town" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
+     
       Home page
     </div>
   )
